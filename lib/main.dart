fe18bdafb775c49adf72cdcf36352c8d6345c586
ranggaadii <ranggapradana161@gmail.com>
2022-02-10 22:09:51 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:absenn/page/login.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Absence App',
+      theme: ThemeData(
+          fontFamily: 'Poppins',
+          scaffoldBackgroundColor: Colors.white,
+          canvasColor: Colors.transparent),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/page/login.dart
@@ -0,0 +1,156 @@
+import 'package:absenn/page/register.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _showPassword = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                child: Text(
+                  "Selamat Datang",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      fontFamily: 'Poppins'),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0),
+                child: Image.asset('assets/icons/login.png'),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    hintText: 'NIS',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(12.0),
+                      ),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(34, 87, 126, 1),
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 30.0),
+                child: TextField(
+                  obscureText: !this._showPassword,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.vpn_key),
+                    hintText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        this._showPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(
+                            () => this._showPassword = !this._showPassword);
+                      },
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(12.0),
+                      ),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(34, 87, 126, 1),
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                child: SizedBox(
+                  width: 370.0,
+                  height: 50.0,
+                  child: RaisedButton(
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                    color: Color.fromRGBO(34, 87, 126, 1),
+                    textColor: Colors.white,
+                    onPressed: () {
+                      // Navigator.of(context).push(MaterialPageRoute(
+                      //     builder: (context) => HomePage(),
+                      //     ));
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      side: BorderSide(color: Color.fromRGBO(34, 87, 126, 1)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 0.0),
+                child: RichText(
+                  text: TextSpan(
+                    text: "Don’t have an account?",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "Sign Up",
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterPage()),
+                            );
+                          },
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
